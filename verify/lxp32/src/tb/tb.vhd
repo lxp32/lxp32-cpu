1ccfbcb7e6447436742c79b3a07ccba273e101d4
Alex I. Kuznetsov <catharanthus@users.noreply.github.com>
2018-10-29 12:08:54 +0300

    verify: add test019 to the list

--- verify/lxp32/src/tb/tb.vhd
+++ verify/lxp32/src/tb/tb.vhd
@@ -133,6 +133,7 @@ begin
 		run_test("test016.ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 		run_test("test017.ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 		run_test("test018.ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
+		run_test("test019.ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 	else
 		run_test(TEST_CASE,clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 	end if;

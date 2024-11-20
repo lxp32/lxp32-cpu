f0b62c71fe76b58dfd92da58b879568e7bb24e43
catharanthus <catharanthus@users.noreply.github.com>
2024-11-21 00:47:55 +0100

    improve test022

--- verify/lxp32/src/firmware/test022.asm
+++ verify/lxp32/src/firmware/test022.asm
@@ -8,14 +8,20 @@
 	lc r101, halt
 	lc r102, failure
 	lc r103, 0
+	lc r104, next_instr_1
+	lc r105, next_instr_2
 
 	lcs r0, func_r200
 	xcall r200, r0
 
+next_instr_1:
 	lcs r0, func_r201
 	xcall r201, r0
 
+next_instr_2:
 	cjmpne r102, r103, 3 // failure
+	cjmpne r102, r200, r104 // failure
+	cjmpne r102, r201, r105 // failure
 
 	sw r100, 1
 	jmp r101 // halt

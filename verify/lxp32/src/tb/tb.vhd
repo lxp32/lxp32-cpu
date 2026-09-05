c15bc3f9c62d39f9efaf8346e1e4d77a0bf1a558
catharanthus <catharanthus@users.noreply.github.com>
2026-09-05 16:01:38 +0200

    tb: don't need to supply extension for the TEST_CASE generic

--- verify/lxp32/src/tb/tb.vhd
+++ verify/lxp32/src/tb/tb.vhd
@@ -137,7 +137,7 @@ begin
 		run_test("test021.ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 		run_test("test022.ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 	else
-		run_test(TEST_CASE,clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
+		run_test(TEST_CASE&".ram",clk,globals,soc_wbs_in,soc_wbs_out,monitor_out);
 	end if;
 	
 	report "ALL TESTS WERE COMPLETED SUCCESSFULLY";
